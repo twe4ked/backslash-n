a5b9d27e025de9a998866ea0cfce7fc41d5aa37f
Odin Dutton <odindutton@gmail.com>
2015-10-27 09:48:38 +1100

    Improve tests

--- test.sh
+++ test.sh
@@ -1,26 +1,67 @@
 #!/bin/bash -e
 
 main() {
-  make
+  build
 
-  echo -n foo > test.txt
-  echo -n bar > test2.txt
+  echo it works with a single file
+  setup
+  ./backslash-n "$FILE_1"
+  diff -w <(hexdump "$FILE_1") <(cat <<EOF
+    0000000 66 6f 6f 0a
+    0000004
+EOF
+)
+  teardown
 
-  state
+  echo it doesn\'t add more than one trailing new line
+  setup
+  ./backslash-n "$FILE_1"
+  ./backslash-n "$FILE_1"
+  diff -w <(hexdump "$FILE_1") <(cat <<EOF
+    0000000 66 6f 6f 0a
+    0000004
+EOF
+)
+  teardown
 
-  ./backslash-n test.txt
+  echo it works with multiple files
+  setup
+  ./backslash-n "$FILE_1" "$FILE_2"
+  diff -w <(hexdump "$FILE_1") <(cat <<EOF
+    0000000 66 6f 6f 0a
+    0000004
+EOF
+)
+  diff -w <(hexdump "$FILE_2") <(cat <<EOF
+    0000000 62 61 72 0a
+    0000004
+EOF
+)
+  teardown
 
-  state
+  echo it returns error if file not found
+  diff -w <(./backslash-n no-file) <(cat <<EOF
+    ./backslash-n: could not open file: no-file
+EOF
+)
 
-  ./backslash-n test.txt test2.txt
-
-  state
+  echo it returns usage
+  diff -w <(./backslash-n) <(echo "usage: ./backslash-n [file ...]")
 }
 
-state() {
-  echo
-  hexdump test.txt
-  hexdump test2.txt
+setup() {
+  FILE_1="$(mktemp -t backslash-n)"
+  FILE_2="$(mktemp -t backslash-n)"
+  echo -n foo > "$FILE_1"
+  echo -n bar > "$FILE_2"
+}
+
+teardown() {
+  rm "$FILE_1" "$FILE_2"
+}
+
+build() {
+  make -s
 }
 
 main
